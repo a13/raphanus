cdc552af99fa426a365ed532c2ab030505fca279
Andrew Rudenko <ceo@prepor.ru>
2015-07-28 12:53:28 +0300

    Version 0.1.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject raphanus "0.1.8-SNAPSHOT"
+(defproject raphanus "0.1.8"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
