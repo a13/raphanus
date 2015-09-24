9b069a2ef8412baf4ff755daf5368c29ffcd2de0
Andrew Rudenko <ceo@prepor.ru>
2015-09-24 19:26:39 +0300

    Version 0.1.17-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject raphanus "0.1.16"
+(defproject raphanus "0.1.17-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
